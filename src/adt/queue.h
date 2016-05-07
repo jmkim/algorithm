586993c2aef69bec6c3c02c196b07d2342ea4c63
Jongmin Kim <jmkim@pukyong.ac.kr>
2016-05-07 20:18:55 +0900

    Refactor the Queue to use doubly linked list
    Implemented as macro

--- src/adt/queue.h
+++ src/adt/queue.h
@@ -3,6 +3,8 @@
  *  ADT: Queue
  *  Declaration
  *
+ *  Required: list.h
+ *
  *  http://github.com/kdzlvaids/algorithm
  *
  */
@@ -10,68 +12,26 @@
 #ifndef ALGORITHM_ADT_QUEUE_H_
 #define ALGORITHM_ADT_QUEUE_H_ 1
 
+#include "list.h"
+
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-#ifndef ADT_QUEUE_VALUE_TYPE
-#define ADT_QUEUE_VALUE_TYPE   void*
-#endif
+typedef adt_list                        adt_queue;
 
-typedef ADT_QUEUE_VALUE_TYPE    adt_queue_value_type;
+#define adt_queue_front(queue)          queue->front_->element_
+#define adt_queue_back(queue)           queue->back_->element_
 
-typedef size_t                  adt_queue_size_type;
-typedef int                     adt_queue_boolean_type;
-#ifndef FALSE
-#   define FALSE    0
-#endif
-#ifndef TRUE
-#   define TRUE     1
-#endif
+#define adt_queue_empty(queue)          (adt_queue_boolean_type)(queue->front_ == NULL)
+#define adt_queue_size(queue)           queue->size_
 
-struct adt_queue_node
-{
-            adt_queue_value_type        element_;
-    struct  adt_queue_node*             next_;
-};
+#define adt_queue_push(queue, element)  adt_list_push_back(queue, element)
+#define adt_queue_pop(queue)            adt_list_pop_front(queue)
 
-typedef struct  adt_queue_node          adt_queue_node_type;
-typedef         adt_queue_node_type*    adt_queue_pointer;
-typedef const   adt_queue_node_type*    adt_queue_const_pointer;
-
-struct adt_queue_adt_queue
-{
-    adt_queue_size_type     size_;
-    adt_queue_pointer       front_;
-    adt_queue_pointer       back_;
-};
-
-typedef struct  adt_queue_adt_queue*  adt_queue;
-
-adt_queue
-adt_queue_create(void);
-
-void
-adt_queue_destroy(adt_queue queue);
-
-adt_queue_boolean_type
-adt_queue_empty(adt_queue queue);
-
-adt_queue_size_type
-adt_queue_size(adt_queue queue);
-
-adt_queue_value_type
-adt_queue_peek(adt_queue queue);
-
-void
-adt_queue_push(adt_queue queue, adt_queue_value_type element);
-
-void
-adt_queue_pop(adt_queue queue);
-
-adt_queue_value_type
-adt_queue_peek_pop(adt_queue queue);
+#define adt_queue_create()              adt_list_create()
+#define adt_queue_destroy(queue)        adt_list_destroy(queue)
 
 #ifdef __cplusplus
 }
