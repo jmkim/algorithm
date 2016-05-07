df8507cd16e12f9f3a8c2111f40abdc6d3cfd45b
Jongmin Kim <jmkim@pukyong.ac.kr>
2016-05-07 20:19:36 +0900

    Refactor the Stack to use doubly linked list
    Implemented as macro
    Bundle with commit 586993c2aef69bec6c3c02c196b07d2342ea4c63

--- src/adt/stack.h
+++ src/adt/stack.h
@@ -10,67 +10,25 @@
 #ifndef ALGORITHM_ADT_STACK_H_
 #define ALGORITHM_ADT_STACK_H_ 1
 
+#include "list.h"
+
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-#ifndef ADT_STACK_VALUE_TYPE
-#define ADT_STACK_VALUE_TYPE   void*
-#endif
+typedef adt_list                        adt_stack;
 
-typedef ADT_STACK_VALUE_TYPE    adt_stack_value_type;
+#define adt_stack_top(stack)            stack->back_->element_
 
-typedef size_t                  adt_stack_size_type;
-typedef int                     adt_stack_boolean_type;
-#ifndef FALSE
-#   define FALSE    0
-#endif
-#ifndef TRUE
-#   define TRUE     1
-#endif
+#define adt_stack_empty(stack)          (adt_stack_boolean_type)(stack->front_ == NULL)
+#define adt_stack_size(stack)           stack->size_
 
-struct adt_stack_node
-{
-            adt_stack_value_type        element_;
-    struct  adt_stack_node*             next_;
-};
+#define adt_stack_push(stack, element)  adt_list_push_back(stack, element)
+#define adt_stack_pop(stack)            adt_list_pop_back(stack)
 
-typedef struct  adt_stack_node          adt_stack_node_type;
-typedef         adt_stack_node_type*    adt_stack_pointer;
-typedef const   adt_stack_node_type*    adt_stack_const_pointer;
-
-struct adt_stack_adt_stack
-{
-    adt_stack_size_type     size_;
-    adt_stack_pointer       top_;
-};
-
-typedef struct  adt_stack_adt_stack*  adt_stack;
-
-adt_stack
-adt_stack_create(void);
-
-void
-adt_stack_destroy(adt_stack stack);
-
-adt_stack_boolean_type
-adt_stack_empty(adt_stack stack);
-
-adt_stack_size_type
-adt_stack_size(adt_stack stack);
-
-adt_stack_value_type
-adt_stack_peek(adt_stack stack);
-
-void
-adt_stack_push(adt_stack stack, adt_stack_value_type element);
-
-void
-adt_stack_pop(adt_stack stack);
-
-adt_stack_value_type
-adt_stack_peek_pop(adt_stack stack);
+#define adt_stack_create()              adt_list_create()
+#define adt_stack_destroy(stack)        adt_list_destroy(stack)
 
 #ifdef __cplusplus
 }
